26d560f505d3ab0c65eaff1c3cc5e0012aa8d03b
RickyIza <rck.ikbm@hotmail.com>
2022-08-26 18:54:28 -0500

    Agregar foto

---
+++ vistas/modulos/404.php
@@ -0,0 +1,34 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Pagina no encontrada
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="Inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Pagina no encontrada</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+    <div class="error-page">
+    <h2 class="headline text-primary">404</h2>
+    <div class="error-content">
+    </div>
+    <h3><i class="fa fa-warning text-primary"></i>
+    Oops! Pagina no encontrada.</h3>
+    <p>
+            Ingresa el menu lateral y alli podras encontrar las paginas 
+            disponibles.
+            Tambien puedes recargar haciendo <a href="inicio">clic aqui</a>
+    </p>
+
+    </div>
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2022 <a href="https://www.tutorialesatualcance.com" target="_blank">Desarrollador Ricky Iza</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
